587e8077bb4c29c41584742d890988f041fd500e
yashablack <iamyashablack@gmail.com>
2023-06-19 18:38:30 +0300

    Correct 1.0.0 schema

--- src/utils/parseCardanoTokenRegistry.js
+++ src/utils/parseCardanoTokenRegistry.js
@@ -1,30 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
-const NAME_PATTERN = /\$?[ \w.'+\-%\/À-ÖØ-öø-ÿ:&\[\]\(\)]+$/gy
-const TICKER_PATTERN = /^[a-zA-Z0-9+\-%/$.]+$/gy
-
 module.exports = function parseCardanoTokenRegistry() {
     let list = [];
 
-    fs.readdirSync(path.join(__dirname, "../../node_modules/cardano-token-registry/mappings")).forEach((fileName, index) => {
+    fs.readdirSync(path.join(__dirname, "../../node_modules/cardano-token-registry/mappings")).forEach((fileName) => {
         const file = fs.readFileSync(path.join(__dirname, `../../node_modules/cardano-token-registry/mappings/${fileName}`));
         const json = JSON.parse(file.toString())
 
-        let policyId = '';
         const token = {};
 
         if (json.subject) {
-            const pid = json.subject.slice(0, 56);
-            token.policyId = pid;
+            token.policyId = json.subject.slice(0, 56);;
             token.subject = json.subject;
-            policyId = pid;
         } else {
             return;
         }
 
-        if (json.name && json.name.value && json.name.value.length <= 40 && NAME_PATTERN.test(json.name.value)
-            && json.ticker && json.ticker.value && TICKER_PATTERN.test(json.ticker.value)
+        if (json.name && json.name.value && json.name.value.length <= 40
+            && json.ticker && json.ticker.value
             && json.decimals && json.decimals.value !== undefined
         ) {
 
